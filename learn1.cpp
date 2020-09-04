2338090c62972df1addc9983f13833b179a60d6b
LionKingSmasher <38488586+LionKingSmasher@users.noreply.github.com>
2020-09-04 18:56:30 +0900

    Initial commit

---
+++ learn1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+template<typename T> //템플릿 설정
+T sum(T a, T b){
+	return a+b;
+}
+
+int main(){
+	std::cout << "Test" << std::endl;
+	std::cout << "템플릿 함수 int형: " << sum<int>(1, 2) << std::endl;
+	std::cout << "템플릿 함수 float형: " << sum<float>(1.21, 2.88) << std::endl;
+	std::cout << "템플릿 함수 string형: " << sum<std::string>("오", "레오") << std::endl;
+}
