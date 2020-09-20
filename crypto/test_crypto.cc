05de54f00d8e7283c47828e550659ab57095b8a4
LionKingSmasher <38488586+LionKingSmasher@users.noreply.github.com>
2020-09-20 21:25:16 +0900

    암호화 연구질

--- crypto/test_crypto.cc
+++ crypto/test_crypto.cc
@@ -6,6 +6,23 @@
 #include <array>
 #include <cmath>
 
+
+uint32_t rotateRight(uint32_t x, uint32_t n) {
+
+    //if n=4, x=0x12345678:
+
+    //shifted = 0x12345678 >> 4 = 0x01234567
+    uint32_t shifted = x >> n;
+
+    //rot_bits = (0x12345678 << 28) = 0x80000000
+    uint32_t rot_bits = x << (32-n);
+
+    //combined = 0x80000000 | 0x01234567 = 0x81234567
+    uint32_t combined = shifted | rot_bits;
+
+    return combined;
+}
+
 std::array<uint32_t, 64> K(){
   double PrimeList[] = {
     2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
@@ -28,25 +45,60 @@ std::array<uint32_t, 64> K(){
   return return_K;
 }
 
-uint32_t S(char in, uint32_t a, uint32_t b, uint32_t c, uint32_t d) {
-  return (~in & a) ^ (in & ~b) ^ (~in & ~c) ^ ~(in & d)
+uint32_t Rotate(std::array<uint32_t, 64> Key){ 
+  for(int i = 0; i<64; i+=8){
+    Key[i] =
+  }
 }
 
-std::array<uint32_t, 64> Crypt(char *input, std::array<uint32_t, 64>) {
-  std::array<uint32_t, 64> return_crypto;
-  for(int i = 0; i < strlen(input); i++){
-    switch(i%4){
-    case 0:
+uint32_t S(char in, uint32_t a, uint32_t b, uint32_t c, uint32_t d) {
+  return (~in & a) ^ (in & ~b) ^ (~in & ~c) ^ ~(in & d);
+}
 
-      break;
+uint32_t S1(char in, uint32_t a, uint32_t b, uint32_t c, uint32_t d){
+  return rotateRight(a, 3) ^ ~rotateRight(b, 30) ^ (c | ~d) | in;
+}
+
+uint32_t S2(char in, uint32_t a, uint32_t b, uint32_t c, uint32_t d){
+  return rotateRight((in | a), (a >> 28)) | ((c | d) % d);
+}
+
+std::array<uint32_t, 64> Crypt(char *input, std::array<uint32_t, 64> Key) {
+  std::array<uint32_t, 64> return_crypto;
+  for(int j = 0; j<256; j++){
+    for(int i = 0; i < 64; i++){
+      switch(i%4){
+      case 0:
+        return_crypto[i] = S(input[i%strlen(input)], Key[i%64], Key[(i+1)%64], Key[(i+2)%64], Key[(i+3)%64]);
+        return_crypto[(i+1)%64] = return_crypto[i];
+        return_crypto[i] = S1(input[i%strlen(input)], return_crypto[i], Key[(i+4)%64], Key[(i+5)%64], Key[(i+6)%64]);
+        break;
+      case 1:
+        return_crypto[i] = S1(input[i%strlen(input)], Key[i%64], Key[(i+3)%64], Key[(i+5)%64], Key[(i+7)%64]);
+        return_crypto[(i+2)%64] = return_crypto[i];
+        return_crypto[(i+1)%64] = S2(input[i%strlen(input)], return_crypto[i], Key[(i+2)%64], Key[(i+4)%64], Key[(i+6)%64]);
+        break;
+      case 2:
+        return_crypto[i] = S2(input[i%strlen(input)], return_crypto[(i-1)%64], Key[(i+1)%64], return_crypto[(i-2)%64], Key[(i+2)%64]);
+        return_crypto[i] <<= 3;
+        return_crypto[(i+1)%64] = S(input[i%strlen(input)], return_crypto[i], return_crypto[(i-1)%64], return_crypto[(i-2)%64], return_crypto[(i-3)%64]);
+        break;
+      case 3:
+        return_crypto[i] = S2(input[i%strlen(input)], return_crypto[(i-1)%64], Key[(i-2)%64], return_crypto[(i-3)%64], Key[(i-4)%64]);
+        break;
+      }
     }
+    Key = return_crypto;
   }
   return return_crypto;
 }
 
 int main(){
+  char input[40];
+  printf("입력: ");
+  scanf("%s", input);
   for(int i = 0; i < 64; i++){
-    printf("%x ", KEY(K())[i]);
+    printf("%x", Crypt(input, K())[i]);
   }
   printf("\n");
 }
