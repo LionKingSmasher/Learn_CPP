52ff8d905367c44711fab599a20f844d4f0c1cc1
LionKingSmasher <38488586+LionKingSmasher@users.noreply.github.com>
2020-09-21 12:10:33 +0900

    익명 함수(람다 함수) 예습
    재밌었다

--- learn4.cc
+++ learn4.cc
@@ -1,5 +1,26 @@
+//20200921 익명 함수 실습
 #include <iostream>
+#include <functional>
+
+int test_lambda(int a, std::function<int(int)> f){
+  return f(a);
+}
+
+int add(int a, int b){
+  return a + b;
+}
 
 int main(){
-
+  int a, b, result;
+  std::cin >> a;
+  /*
+  auto calc = add(a, [&b](void) -> int {
+    return b *= b;
+  });
+  std::cout << calc << std::endl;
+  */
+  result = test_lambda(a, [](int a) -> int {
+    a *= a;
+  });
+  std::cout << result << std::endl;
 }
