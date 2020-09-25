6767c0c57d12d170d5acc03a5adf8244af1f3a13
LionKingSmasher <38488586+LionKingSmasher@users.noreply.github.com>
2020-09-25 09:32:33 +0900

    공부하고 있는 것

--- learn5.cc
+++ learn5.cc
@@ -27,9 +27,11 @@ struct TEST{
   template<typename ...T>
   TEST(T... args) : d{args... } {}
   float operator[] (unsigned n) const { return d[n]; } //n은 배열 내의 인덱스 번호를 의미
+  inline TEST<3> add(TEST<3> b) { return TEST<3>{d[0]+b[0], d[1]+b[1], d[2]+b[2]}; }
 };
 
 int main(){
-  TEST<3> xyz{1.f, 2.f, 3.f};
-  std::cout << xyz[1] << std::endl;
+  TEST<3> xyz{1.f, 2.f, 3.f}, xyz2{2.f, 3.f, 4.f};
+  TEST<3> result = xyz.add(xyz2); // result[0]=3.0, result[1]=5.0, result[2]=7.0
+  std::cout << result[0] << std::endl; //출력 결과 3(3.0)
 }
