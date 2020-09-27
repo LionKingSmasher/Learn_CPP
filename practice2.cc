bf048fff52ac17a7f8ff4256f3645a660d14d2ce
LionKingSmasher <38488586+LionKingSmasher@users.noreply.github.com>
2020-09-27 14:09:22 +0900

    응용 연습

---
+++ practice2.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cinttypes>
+
+typedef uint_least32_t u32;
+
+template<unsigned no, typename T=u32>
+struct MATH
+{
+  u32 imaginary = 0;
+  u32 real = 0;
+  MATH(u32 i, u32 r) : imaginary(i), real(r) { }
+  MATH& operator+(MATH& second)
+  {
+    imaginary += second.imaginary;
+    real += second.real;
+    return *this;
+  }
+};
+
+/*
+template<unsigned no, typename T=u32>
+struct Test{
+  T data = 0;
+  Test& operator=(int val)
+  {
+    data = val;
+    return *this;
+  }
+};
+*/
+
+int main(){
+  MATH<0> first(0, 3), second(4, 2);
+  MATH<0> result = first + second;
+  std::cout << "<" << first.imaginary << "," << result.real << ">" << std::endl;
+}
