9c35f75e726faef3cfdaef552e7d6428910e31c7
LionKingSmasher <38488586+LionKingSmasher@users.noreply.github.com>
2020-09-30 20:46:24 +0900

    오늘 개발 한 것

--- virus/casino.cc
+++ virus/casino.cc
@@ -3,17 +3,28 @@
 
 struct virus {
   char input[3] = {0,};
+  int coin=5;
+  operator unsigned() const { return coin; }
 };
 
 int main(){
   char input;
+  struct virus v;
   system("clear");
   printf("카지노를 시작하겠습니다? [Y|N]: ");
   scanf("%c", &input);
   if(input=='N'||input=='n') exit(0);
-  system("clear");
-  printf("----------------------Welcome to Casino----------------------\n");
-  printf("                    \u250F\u2501\u2513      \u250F\u2501\u2513      \u250F\u2501\u2513\n");
-  printf("                    \u254A%c\u2549      \u254A%c\u2549      \u254A%c\u2549\n", 'a', 'b', 'c');
-  printf("                    \u2517\u2501\u251B      \u2517\u2501\u251B      \u2517\u2501\u251B\n");
+  while(true){
+    system("clear");
+    printf("----------------------Welcome to Casino----------------------\n");
+    printf("                    \u250F\u2501\u2513      \u250F\u2501\u2513      \u250F\u2501\u2513\n");
+    printf("                    \u254A%c\u2549      \u254A%c\u2549      \u254A%c\u2549\n", 'a', 'b', 'c');
+    printf("                    \u2517\u2501\u251B      \u2517\u2501\u251B      \u2517\u2501\u251B\n");
+    printf("남은 코인 갯수: %d\n", v.coin);
+    std::endl;
+    std::endl;
+    if(coin<=0) exit(0);
+    printf("돌린다[Y|N]");
+    scanf("%c", input);
+  }
 }
