aa8d079b750fab74a1386e5f28fd07e49ce8deb8
LionKingSmasher <38488586+LionKingSmasher@users.noreply.github.com>
2020-10-02 16:33:49 +0900

    BF 본격 개발 시작

--- BF/BruteForce.s
+++ BF/BruteForce.s
@@ -2,6 +2,23 @@
 global BF
 section .text
 BF:
-	mov rax, rdi
-	mov byte [rax+2], 67
+	xor rax, rax ;0으로 초기화
+	xor r10, r10 ;브루트 포스 대상 문자열 길이
+	xor r11, r11 ;브루트 포스 rcx 카운터
+	xor r9, r9 ;브루트포스 대상 문자열
+	mov r8, rdi ;문자열
+	mov rcx, rsi ;문자열 길이
+	mov byte [r9], 0
+
+start:
+	add byte [r9+r10], 1
+	jmp cmp_
+
+done:
 	ret
+
+cmp_:
+	cmp byte [r9+r11], byte [r8+r11]
+	jne start
+	inc r11
+	loop cmp_
