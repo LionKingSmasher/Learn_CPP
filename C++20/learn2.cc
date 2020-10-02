dbebaae562ba959a3ccb2d04fc75ba8cdee7c1b7
LionKingSmasher <38488586+LionKingSmasher@users.noreply.github.com>
2020-10-03 00:40:20 +0900

    우주선 연산자 예제 추가

--- C++20/learn2.cc
+++ C++20/learn2.cc
@@ -1,5 +1,15 @@
 #include <iostream>
 #include <compare>
+/*
+int main()
+{
+   int a, b;
+   std::cin >> a >> b;
+   if((a<=>b)>0) std::cout << "오호 이런거군" << std::endl;
+   return 0;
+}
+*/
+
 
 int main(){
   int a, b;
