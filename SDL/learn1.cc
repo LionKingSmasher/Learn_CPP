cb7737b2b84b3f9c41a07b154e01c82902e921be
LionKingSmasher <38488586+LionKingSmasher@users.noreply.github.com>
2020-10-07 22:22:53 +0900

    Update learn1.cc

--- SDL/learn1.cc
+++ SDL/learn1.cc
@@ -1,31 +1,25 @@
 #include <SDL2/SDL.h>
-#include <GL/gl.h>
-#include <GL/glu.h>
-#include <GLFW
 
-int main()
-{
-    SDL_Window *win = NULL;
-    SDL_GLContext context;
+int main(int argc, char *argv[]){
+    SDL_Window *screen;
+    SDL_Renderer *renderer;
+    SDL_Init(SDL_INIT_VIDEO);
+    screen = SDL_CreateWindow("TEST", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, 0);
+    renderer = SDL_CreateRenderer(screen, -1, 0);
 
-    SDL_Init(SDL_INIT_VIDEO); //SDL 초기화
-    win = SDL_CreateWindow("TEST", 0, 0, 480, 800, SDL_WINDOW_OPENGL); //윈도우 생성
-    context = SDL_GL_CreateContext(win);
-
-    SDL_bool isQuit = SDL_FALSE;
-    while(!isQuit)
+    bool quit = false;
+    SDL_Event event;
+    while (!quit)
     {
-        SDL_Event e;
-        if(SDL_PollEvent(&e)){
-            if(e.type == SDL_QUIT)
+        while(SDL_PollEvent(&event)){
+            switch (event.type)
             {
-                isQuit = SDL_TRUE;
+                case SDL_QUIT:
+                    quit = true;
+                    break;
             }
         }
-        static const GLfloat red[] = {1.0f, 0.0f, 0.0f, 1.0f};
-        glClearBufferfv(GL_COLOR, 0, red);
-        SDL_GL_SwapWindow(win);
+        SDL_Delay(1);
     }
     SDL_Quit();
-    return 0;
 }
