ff259a8b1821143bb8f2c4a513a0806dfd6588c9
LionKingSmasher <38488586+LionKingSmasher@users.noreply.github.com>
2020-10-07 21:00:10 +0900

    예제2

--- learn9.cc
+++ learn9.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 
+/*
 constexpr int sum(int n){
     int result = 0;
     for(int i = 1; i<=n; i++){
@@ -17,4 +18,35 @@ struct A{
 int main(){
     struct A<sum(100)> a;
     std::cout << a << std::endl;
+}
+*/
+
+template<int ex>
+struct A{
+    int data[ex];
+    template<typename ...T>
+    A(T... args) : data{args...} { }
+    auto add(int n){
+        if constexpr (ex == 1){
+            return data[0] += n;
+        }
+        if constexpr (ex == 2){
+            data[0] += n;
+            data[1] += data[0];
+            return A<2>{data[0], data[1]};
+        }
+        if constexpr (ex == 3){
+            data[0] += n;
+            data[1] += data[0];
+            data[2] += data[1] + data[0];
+            return A<3>{data[0], data[1], data[2]};
+        }
+    }
+};
+
+int main(){
+    struct A<3> a{1, 2, 3};
+    std::cout << a.data[2] << std::endl;
+    struct A<3> b = a.add(1);
+    std::cout << b.data[2] << std::endl;
 }
