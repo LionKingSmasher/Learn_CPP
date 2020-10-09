d4e35bdbbd1ce7b2c805f32faeb90f2cb38edf0a
LionKingSmasher <38488586+LionKingSmasher@users.noreply.github.com>
2020-10-09 23:29:53 +0900

    흥미로운 결과

--- learn10.cc
+++ learn10.cc
@@ -1,7 +1,14 @@
 #include <cstdio>
+#include <iostream>
 
 int main(){
-    int a = (int){
-        1
-    };
+    int a = ({
+        std::cout << "Test" << std::endl;
+        int a;
+        std::cin >> a;
+        a++;
+        std::cout << a << std::endl;
+        a;
+    });
+    std::cout << a << std::endl;
 }
